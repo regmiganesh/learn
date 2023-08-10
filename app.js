a7c63f5845d623a5019739860342a73d837ede46
Ganesh Regmi <98951707+regmiganesh@users.noreply.github.com>
2023-08-10 22:23:50 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+/*
+var tl=gsap.timeline()
+
+tl.from("header nav h1, header nav .nav-link a",{
+    y:-100,
+    duration:1,
+    delay:1,
+    stagger:0.5
+})
+
+const element =document.querySelector(".first-small, .mainh1, .secondpasag");
+
+tl.from(element,{
+    x:-100,
+    
+
+})
+*/
